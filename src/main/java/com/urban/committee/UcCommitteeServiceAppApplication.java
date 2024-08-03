184cbf691d727ba3717562ccb5c2b2df95fd8a3b
Pdubey1994 <committeeurban@gmail.com>
2024-08-03 20:59:26 +0530

    initial commit

---
+++ src/main/java/com/urban/committee/UcCommitteeServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcCommitteeServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcCommitteeServiceAppApplication.class, args);
+    }
+
+}
